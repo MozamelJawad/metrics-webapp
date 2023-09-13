d075a940f56116248322e8a77033d7ed2a483b7c
Mozamel Jawad <mozameljawad@gmail.com>
2023-09-13 18:11:36 +0430

    solve linter errors

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 
 function NotFound() {
   return (
-    <div style={{ color: 'red', textAlign: 'center', marginTop: '1rem' }}>
+    <div className="notfound">
       <h3>Page Not Found</h3>
-      <p style={{ marginTop: '1rem', color: 'blue' }}><Link to="/">Got to Home page</Link></p>
+      <p className="goTo"><Link to="/">Got to Home page</Link></p>
     </div>
   );
 }
